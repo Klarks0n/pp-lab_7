bfcc8d9248022ce991a47b6c906879fdf4bbd294
Klarks0n <165905750+Klarks0n@users.noreply.github.com>
2024-06-01 13:01:13 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,14 +2,18 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class Main extends Application {
     private TextField directoryPathField;
     private TextField searchField;
+    private TextArea resultArea;
 
     @Override
     public void start(Stage primaryStage) {
@@ -25,12 +29,16 @@ public class Main extends Application {
         browseButton.setOnAction(e -> browseDirectory());
 
         Button searchButton = new Button("Search");
+        searchButton.setOnAction(e -> searchFiles());
 
         HBox hBox = new HBox(10, directoryPathField, browseButton);
 
-        VBox vBox = new VBox(10, hBox, searchField, searchButton);
+        resultArea = new TextArea();
+        resultArea.setPrefHeight(400);
 
-        Scene scene = new Scene(vBox, 600, 200);
+        VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
+
+        Scene scene = new Scene(vBox, 600, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -42,6 +50,33 @@ public class Main extends Application {
 
         if (selectedDirectory != null) {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
+        }
+    }
+
+    private void searchFiles() {
+        String directoryPath = directoryPathField.getText();
+        if (directoryPath.isEmpty()) {
+            resultArea.setText("Please provide a directory path.");
+            return;
+        }
+
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            resultArea.setText("The provided path is not a directory.");
+            return;
+        }
+
+        StringBuilder results = new StringBuilder();
+        listFilesInDirectory(directory, results);
+        resultArea.setText(results.toString());
+    }
+
+    private void listFilesInDirectory(File directory, StringBuilder results) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                results.append(file.getPath()).append("\n");
+            }
         }
     }
 
